c7445f18af449f4adba02c5f66f3fb0d7eac4263
koldovsky <koldovsky@yahoo.com>
2018-05-30 15:58:41 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// const userName = prompt('Enter your name!');
+// const greet = 'Hello ' + userName;
+// alert(greet);
+
+// const userAge = parseInt(prompt('Enter your age!'));
+// if (userAge < 18) {
+//     alert('Child');
+// } else if (userAge < 80) {
+//     alert('Adult');
+// } else {
+//     alert('Old');
+// }
+
+// function sum(a, b) {
+//     const result = a + b;
+//     return result;
+// }
+
+// function sumOfThree(a, b, c) {
+//     const result = a + b + c;
+//     return result;
+// }
+
+// let sumOfNums = sum(10, 20);
+// alert(sumOfNums);
+// sumOfNums = sum(20, 30);
+// alert(sumOfNums);
+
+// let sumthree = sumOfThree(1, 2, 3);
+// alert(sumthree);
+
+// const message = 'Вітаємо на нашому сайті!';
+// const greetings = 'Привіт!';
+// alert(greetings);
+// alert(message);
+
+
